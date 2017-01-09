c0bef9fcd6eb2dff7f38becacccd5f6fb4d5d7fc
paulo.ferreira <paulo.ferreira@siteblindado.com.br>
2017-01-09 14:54:07 -0200

    Refactor na função e add do server + static file

--- crawler.js
+++ crawler.js
@@ -1,31 +1,33 @@
+var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-var MAX_PAGES = 1;
-var ACTUAL_PAGE = 1;
+var app = express();
 var DATA = { results: []};
 var URL_STEAM;
 
-function getOffers() {
-    URL_STEAM = 'http://store.steampowered.com/search/?sort_by=Price_ASC&category1=998&specials=1&page=' + ACTUAL_PAGE;
+function getOffers(actualPage = 1) {
+    URL_STEAM = 'http://store.steampowered.com/search/results?sort_by=Price_ASC&category1=998&specials=1&page=' + actualPage;
 
     request(URL_STEAM, function(err, res, body) {
         if(err) console.log('Erro: ' + err);
 
         var $ = cheerio.load(body);
 
-        MAX_PAGES = parseInt($('.search_pagination_right a.pagebtn:last-child').prev().text());
+        var maxPages = parseInt($('.search_pagination_right a.pagebtn:last-child').prev().text());
 
         $('#search_result_container > div > a').each(function() {
+            var link = $(this).attr('href');
             var image = $(this).find('.search_capsule img').attr('src');
             var title = $(this).find('.title').text().trim();
             var discount = $(this).find('.search_discount > span').text().trim();
             var oldPrice = $(this).find('.discounted > span > strike').text().trim();
-            var newPrice = $(this).find('.discounted > span + br')[0].next.data.trim();
+            var newPrice = "R$ " + $(this).find('.discounted').text().trim().split('R$').pop().trim();
 
             DATA.results.push(
                 {
+                    link,
                     image,
                     title,
                     discount,
@@ -37,10 +39,19 @@ function getOffers() {
             fs.writeFile('offers.json', JSON.stringify(DATA));
         });
 
-        if(ACTUAL_PAGE ++ <= MAX_PAGES) {
-            getOffers();
+        if(actualPage <= maxPages) {
+            getOffers(++actualPage);
         }
     });
-}
+};
 
-getOffers();
+app.get('/', function (req, res) {
+    getOffers();
+    res.send("Last update - " + new Date().toLocaleString());
+});
+
+app.use('/api', express.static(__dirname));
+
+app.listen(3000, function () {
+    console.log('Listening on 3000');
+});
