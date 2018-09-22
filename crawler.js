d96f1d8d43d5487b37a83e9e024a093b44d95285
Paulo Ferreira <Paulo.Ferreira@saiglobal.com>
2018-09-22 19:35:59 -0300

    Use pagination and API instead files

--- crawler.js
+++ crawler.js
@@ -1,59 +1,59 @@
 var express = require('express');
-var request = require('request');
+var promise = require('request-promise');
 var cheerio = require('cheerio');
-var cors = require('cors')
-var fs = require('fs');
+var cors = require('cors');
 
 var app = express();
-var DATA = { results: []};
-var URL_STEAM;
 
-function getOffers(actualPage = 1) {
-    URL_STEAM = 'http://store.steampowered.com/search/results?sort_by=Price_ASC&category1=998&specials=1&cc=br&page=' + actualPage;
+function getOffers(page) {
+    var DATA = { results: [] };
+    var URL_STEAM = 'http://store.steampowered.com/search/results?sort_by=Price_ASC&category1=998&specials=1&cc=br&page=' + page;
 
-    request(URL_STEAM, function(err, res, body) {
-        if(err) console.log('Erro: ' + err);
-
-        var $ = cheerio.load(body);
-
-        var maxPages = parseInt($('.search_pagination_right a.pagebtn:last-child').prev().text());
-
-        $('#search_result_container > div > a').each(function() {
-            var link = $(this).attr('href');
-            var image = $(this).find('.search_capsule img').attr('src');
-            var title = $(this).find('.title').text().trim();
-            var discount = $(this).find('.search_discount > span').text().trim();
-            var oldPrice = $(this).find('.discounted > span > strike').text().trim();
-            var newPrice = "R$ " + $(this).find('.discounted').text().trim().split('R$').pop().trim();
-
-            DATA.results.push(
-                {
-                    link,
-                    image,
-                    title,
-                    discount,
-                    oldPrice,
-                    newPrice
-                }
-            );
-
-            fs.writeFile('offers.json', JSON.stringify(DATA));
-        });
-
-        if(actualPage <= maxPages) {
-            getOffers(++actualPage);
+    var request = {
+        method: 'GET', 
+        uri: URL_STEAM,
+        transform: function (body) {
+            return cheerio.load(body);
         }
-    });
+    };
+
+    return promise(request)
+        .then(function($) {
+            $('#search_result_container > div > a').each(function() {
+                var link = $(this).attr('href');
+                var image = $(this).find('.search_capsule img').attr('src');
+                var title = $(this).find('.title').text().trim();
+                var discount = $(this).find('.search_discount > span').text().trim();
+                var oldPrice = $(this).find('.discounted > span > strike').text().trim();
+                var newPrice = "R$ " + $(this).find('.discounted').text().trim().split('R$').pop().trim();
+
+                DATA.results.push(
+                    {
+                        link,
+                        image,
+                        title,
+                        discount,
+                        oldPrice,
+                        newPrice
+                    }
+                );
+            });
+        })
+        .then(function() { return JSON.stringify(DATA) })
+        .catch(function(err) {
+            console.log('Erro: ' + err);
+        });
 };
 
 app.use(cors());
 
 app.get('/', function (req, res) {
-    getOffers();
-    res.send("Last update - " + new Date().toLocaleString());
+    res.setHeader('Content-Type', 'application/json');
+    getOffers(req.query.page)
+        .then(function(offers) {
+            res.send(offers);
+        });
 });
-
-app.use('/api', express.static(__dirname));
 
 var port = process.env.PORT || 8080;
 
